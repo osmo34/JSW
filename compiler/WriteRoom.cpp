8d04cabde50ff4bae3404bc59c5ce35de755b46a
Steve <osmo34@users.noreply.github.com>
2019-01-01 23:09:29 +0000

    Add files via upload
    Re-factored to remove the endless if statement

--- compiler/WriteRoom.cpp
+++ compiler/WriteRoom.cpp
@@ -4,6 +4,47 @@ WriteRoom::WriteRoom() {}
 WriteRoom::~WriteRoom() {}
 
 
+void WriteRoom::updateRoomData(RoomData *roomData, std::string textType, std::string value)
+{
+	types type;
+	std::string output;
+	int id;	
+
+	// floats that represent the roomData data structure, anything that isn't a float and is handled elsewhere is represented by a 0
+	float roomTypes[20] = {0.0f, 0.0f, 0.0f, 0.0f, roomData->positionX, roomData->positionY, roomData->speedX, roomData->speedY, 0.0f,
+								roomData->clampXLeft, roomData->clampXRight, roomData->clampYTop, roomData->clampYBottom, roomData->stairBottomX,
+								roomData->stairBottomY, roomData->stairTopX, roomData->stairTopY, roomData->ropeX, roomData->ropeY, roomData->ropeTopY};
+
+	// work out which bit of roomData we are working with
+	for (int i = 0; i < sizeof(type.type) / sizeof(type.type[0]); i++) {
+		if (textType == type.type[i]) {
+			output = type.type[i];
+			id = i;
+			roomTypes[id] = std::stof(value);
+			break;
+		}
+	}
+
+	// re-copy all the data back to the pointer so it's not lost with scope
+	roomData->positionX = roomTypes[4];
+	roomData->positionY = roomTypes[5];
+	roomData->speedX = roomTypes[6];
+	roomData->speedY = roomTypes[7];
+	roomData->clampXLeft = roomTypes[9];
+	roomData->clampXRight = roomTypes[10];
+	roomData->clampYTop = roomTypes[11];
+	roomData->clampYBottom = roomTypes[12];
+	roomData->stairBottomX = roomTypes[13];
+	roomData->stairBottomY = roomTypes[14];
+	roomData->stairTopX = roomTypes[15];
+	roomData->stairTopY = roomTypes[16];
+	roomData->ropeX = roomTypes[17];
+	roomData->ropeY = roomTypes[18];
+	roomData->ropeTopY = roomTypes[19];
+	roomData->isTravelator = 0; // make sure we are not a travelator - this is handled elsewhere!
+}
+
+
 Room WriteRoom::createRoomData()
 {
 	Room room{};
@@ -19,7 +60,7 @@ Room WriteRoom::createRoomData()
 	if (!inFile.is_open()) {
 		std::cout << "read failed";
 	}
-	RoomData roomData{};
+	RoomData *roomData = new RoomData;
 	int i = 0;
 
 	while (inFile) {
@@ -28,7 +69,7 @@ Room WriteRoom::createRoomData()
 		std::string value;
 		inFile >> value;
 		inFile >> std::ws;
-		// TODO: Refactor this
+
 		if (type == "num") {
 			room.numObjects = std::stoi(value);
 		}
@@ -39,74 +80,32 @@ Room WriteRoom::createRoomData()
 		else if (type == "type") {
 			char *c = new char[value.length() + 1];
 			strcpy(c, value.c_str());
-			roomData.objectType = *c;
-			delete c;
+			roomData->objectType = *c;
+			delete[] c;
 		}
 		else if (type == "texture") {
-			roomData.textureId = std::stoi(value);
-		}
-		else if (type == "px") {
-			roomData.positionX = std::stof(value);
-		}
-		else if (type == "py") {
-			roomData.positionY = std::stof(value);
-		}
-		else if (type == "sx") {
-			roomData.speedX = std::stof(value);
-		}
-		else if (type == "sy") {
-			roomData.speedY = std::stof(value);
-		}
-		else if (type == "clampXLeft") {
-			roomData.clampXLeft = std::stof(value);
-		}
-		else if (type == "clampXRight") {
-			roomData.clampXRight = std::stof(value);
-		}
-		else if (type == "clampYTop") {
-			roomData.clampYTop = std::stof(value);
-		}
-		else if (type == "clampYBottom") {
-			roomData.clampYBottom = std::stof(value);
-		}
-		else if (type == "stbx") {
-			roomData.stairBottomX = std::stof(value);
-		}
-		else if (type == "stby") {
-			roomData.stairBottomY = std::stof(value);
-		}
-		else if (type == "sttx") {
-			roomData.stairTopX = std::stof(value);
-		}
-		else if (type == "stty") {
-			roomData.stairTopY = std::stof(value);
-		}
-		else if (type == "ropeX") {
-			roomData.ropeX = std::stof(value);
-		}
-		else if (type == "ropeY") {
-			roomData.ropeY = std::stof(value);
-		}
-		else if (type == "ropeTop") {
-			roomData.ropeTopY = std::stof(value);
+			roomData->textureId = std::stoi(value);
 		}
 		else if (type == "travelator") {
 			if (value == "1") {
-				roomData.isTravelator = 1;
+				roomData->isTravelator = 1;
 			}
 			else {
-				roomData.isTravelator = 0;
+				roomData->isTravelator = 0;
 			}
-		}
-
+		}		 
 		else if (type == "end") {
-			room.roomData[i] = roomData;
+			room.roomData[i] = *roomData;
 			i++;
 		}
+		else {			
+			updateRoomData(roomData, type, value);
+		}	
 	}
 
 	inFile.close();
 	std::cout << "Compile complete" << std::endl;
+	delete[] roomData;
 	return room;
 }
 
@@ -129,5 +128,6 @@ void WriteRoom::createRoom(std::string fileName)
 	std::cout << "verification" << std::endl;
 	for (int i = 0; i < 5; i++) {
 		std::cout << room.roomData[i].objectType << std::endl;
+		std::cout << room.roomData[i].positionX << std::endl;
 	}
 }
--- compiler/WriteRoom.h
+++ compiler/WriteRoom.h
@@ -4,10 +4,16 @@
 #include <String>
 #include <iostream>
 #include <cstdint>
+#include <vector>
+#include <memory>
 
 #pragma warning(disable:4996)
 
-// this will at a later stage become a compiler
+struct types {
+	std::string type[20] = { "num", "roomID", "type", "texture", "px", "py", "sx", "sy", "clampXLeft",
+		"clampXRight", "clampYTop", "clampYBottom", "stbx", "stby", "sttx", "stty", "ropeX", "ropeY", "ropeTop", "travelator" };
+};
+
 class WriteRoom
 {
 public:
@@ -17,5 +23,7 @@ public:
 
 private:
 	Room createRoomData();
+	void updateRoomData(RoomData *roomData, std::string textType, std::string value);
+
 };
 
